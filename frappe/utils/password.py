22125c00ded7feb8d508530b6544f8947de05db6
Richard Rattray <richard@parsimony.com>
2025-02-19 21:04:32 -0500

    added regression for password
    This regression is necessary for the passwords on the T&A site to work properly

--- frappe/utils/password.py
+++ frappe/utils/password.py
@@ -35,8 +35,10 @@ def get_decrypted_password(doctype, name, fieldname="password", raise_exception=
 	).run()
 
 	if result and result[0][0]:
-		return decrypt(result[0][0], key=f"{doctype}.{name}.{fieldname}")
-
+		if ("Insight" in doctype or "Email" in doctype) and (frappe.local.conf.user_encryption_key):
+			return decrypt(result[0][0], key=frappe.local.conf.user_encryption_key)
+		else:
+			return decrypt(result[0][0], key=f"{doctype}.{name}.{fieldname}")
 	elif raise_exception:
 		frappe.throw(
 			_("Password not found for {0} {1} {2}").format(doctype, name, fieldname),
